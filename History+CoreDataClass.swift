4c3382e3d3404d4a2f09e67a58589135d83caebe
RRJJ <h19990728@gmail.com>
2019-06-09 04:56:57 +0800

    new page& swift

---
+++ History+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  History+CoreDataClass.swift
+//  UnKnownHand
+//
+//  Created by ohfreeya on 2019/6/8.
+//  Copyright © 2019年 ohfreeya. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+public class History: NSManagedObject {
+
+}
